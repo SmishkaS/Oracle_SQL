8dc56da920202c6ae0e3498cabb1d5ed89d8eb80
Mishka <7822733@mail.ru>
2023-01-26 14:34:54 +0300

    added tasks about SUBQUERY 3

--- more_tasks_with_SELECT.sql
+++ more_tasks_with_SELECT.sql
@@ -124,3 +124,18 @@ WHERE department_id IN (SELECT DISTINCT department_id FROM employees);
 SELECT e1.first_name, e1.last_name, e1.salary FROM employees e1
 WHERE salary > (SELECT AVG(e2.salary) FROM employees e2
 WHERE e2.department_id = e1.department_id);
+
+-- Найдите работников которые работают в Великобритании
+SELECT first_name, last_name FROM employees WHERE department_id IN
+(SELECT department_id FROM departments WHERE location_id IN
+(SELECT location_id FROM locations WHERE country_id =
+(SELECT country_id FROM countries WHERE country_name = 'United Kingdom')));
+
+-- Найдите среди менеджеров кто зарабатывает больше чем средняя зарплата по всей компании
+SELECT first_name, last_name FROM employees WHERE job_id IN
+(SELECT job_id FROM jobs WHERE UPPER(job_title) like '%MANAGER%') AND salary >
+(SELECT AVG(salary) FROM employees);
+
+--Найдите работников которые зарабатывают больше чем David
+SELECT first_name, last_name FROM employees WHERE salary >
+(SELECT MIN(salary) FROM employees where first_name = 'David');
